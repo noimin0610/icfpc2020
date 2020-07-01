c83268c82ab5bbfd3e77700d9e18ecd3539c748e
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2020-07-02 06:45:57 +0900

    Send a sample HTTP request
    The code does roughly the same thing as starterkit-csharp.
    https://github.com/icfpcontest2020/starterkit-csharp/blob/master/app/Program.cs

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
 import sys
 
-print(sys.argv[1] + " " + sys.argv[2])
+import requests
+
+
+def main():
+    server_url = sys.argv[1]
+    player_key = sys.argv[2]
+    print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
+
+    res = requests.get('%s?playerKey=%s' % (server_url, player_key))
+    res.raise_for_status()
+
+
+if __name__ == '__main__':
+    main()
