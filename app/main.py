3bb02272b6b3509e24a335ba61947a3788ffd8e9
matsu7874 <mtsmtkmt@gmail.com>
2020-07-18 11:13:59 +0900

    test aliens api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 
+
 def main():
     server_url = sys.argv[1]
     player_key = sys.argv[2]
@@ -13,6 +14,18 @@ def main():
         print('Response body:', res.text)
         exit(2)
     print('Server response:', res.text)
+
+    aliens_api = server_url + '/aliens/'
+    for i in range(1, 10):
+        res = requests.post(server_url + str(i), data=player_key)
+        if res.status_code == 302:
+            print('HTTP code:', res.status_code)
+        elif res.status_code != 200:
+            print('Unexpected server response:')
+            print('HTTP code:', res.status_code)
+            print('Response body:', res.text)
+            exit(2)
+        print('Server response:', res.text)
 
 
 if __name__ == '__main__':
