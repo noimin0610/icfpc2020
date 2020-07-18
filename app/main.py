260b19684540873223c9f7840ff30fa1ca4aca38
noimin0610 <mii.informationsystems@gmail.com>
2020-07-18 11:43:48 +0900

    delete unnecessary params

--- app/main.py
+++ app/main.py
@@ -7,18 +7,15 @@ def main():
     player_key = sys.argv[2]
     print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
 
-    # res = requests.post(server_url, data=player_key)
-    # if res.status_code != 200:
-    #     print('Unexpected server response:')
-    #     print('HTTP code:', res.status_code)
-    #     print('Response body:', res.text)
-    #     exit(2)
-    # print('Server response:', res.text)
+    res = requests.post(server_url, data=player_key)
+    if res.status_code != 200:
+        print('Unexpected server response:')
+        print('HTTP code:', res.status_code)
+        print('Response body:', res.text)
+        exit(2)
+    print('Server response:', res.text)
 
     aliens_api = server_url + '/aliens/send/'
-    params = {
-        "apiKey": player_key
-    }
     data = "string"
     url = aliens_api
     print('URL:', url)
