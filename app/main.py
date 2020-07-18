ea059d9202e9071dfa1e171c387332d6fe9872b8
noimin0610 <mii.informationsystems@gmail.com>
2020-07-18 12:02:59 +0900

    use /aliens/send

--- app/main.py
+++ app/main.py
@@ -7,19 +7,23 @@ def main():
     player_key = sys.argv[2]
     print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
 
-    res = requests.post(server_url, data=player_key)
-    if res.status_code != 200:
-        print('Unexpected server response:')
-        print('HTTP code:', res.status_code)
-        print('Response body:', res.text)
-        exit(2)
-    print('Server response:', res.text)
+    params = {
+        "apiKey": "fcec2a485b1a4a89898fd2021b88fbd8"
+    }
 
-    aliens_api = server_url + '/aliens/send/'
+    # res = requests.post(server_url, data=player_key, params=params)
+    # if res.status_code != 200:
+    #     print('Unexpected server response:')
+    #     print('HTTP code:', res.status_code)
+    #     print('Response body:', res.text)
+    #     exit(2)
+    # print('Server response:', res.text)
+
+    aliens_api = server_url + '/aliens/send'
     data = "string"
     url = aliens_api
     print('URL:', url)
-    res = requests.post(url, data=data)
+    res = requests.post(url, data=data, params=params)
     if res.status_code == 302:
         print('HTTP code:', res.status_code)
     elif res.status_code != 200:
@@ -27,6 +31,8 @@ def main():
         print('HTTP code:', res.status_code)
         print('Response body:', res.text)
         exit(2)
+        print('Server response:', res.text)
+    else:
         print('Server response:', res.text)
 
 
