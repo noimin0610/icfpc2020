d22dd27d8972a29f082d8012b8a1c11b3b575fe2
matsu7874 <mtsmtkmt@gmail.com>
2020-07-19 17:06:26 +0900

    fix bug

--- app/evaluator.py
+++ app/evaluator.py
@@ -7,6 +7,9 @@ class Expr:
     def __init__(self, evaluated: Optional['Expr'] = None):
         self.evaluated = evaluated
 
+    def __str__(self):
+        return str(self.evaluated)
+
 
 class Atom(Expr):
     def __init__(self, name: str):
@@ -14,7 +17,7 @@ class Atom(Expr):
         self.name = name
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Ap(Expr):
@@ -23,8 +26,8 @@ class Ap(Expr):
         self.fun = fun
         self.arg = arg
 
-    def __str__(self):
-        return 'Ap({},{})'.format(self.fun, self.arg)
+    def __str__(self) -> str:
+        return 'Ap({},{})'.format(str(self.fun), str(self.arg))
 
 
 class Vect:
@@ -33,96 +36,133 @@ class Vect:
         self.y = y
 
 
-cons = Atom("cons")
-t = Atom("t")
-f = Atom("f")
-nil = Atom("nil")
+class AtomList:
+    def __init__(self, lst):
+        self.list = lst
+
+    def __str__(self):
+        return '[{}]'.format(','.join(str(atom) for atom in self.list))
+
+
+cons = Atom('cons')
+t = Atom('t')
+f = Atom('f')
+nil = Atom('nil')
 
 FUNCTIONS: Dict[str, Expr] = dict()
 
 
+def SEND_TO_ALIEN_PROXY(data):
+
+    return [Atom(0)]
+    return [Atom(1), Atom(67425)]
+
+
+def GET_LIST_ITEMS_FROM_EXPR(res):
+    return [Atom(0), AtomList([Atom(1)]), Atom(1)]
+
+
 def interact(state: Expr,  event: Expr) -> Tuple[Expr, Expr]:
     # See https://message-from-space.readthedocs.io/en/latest/message38.html
-    expr: Expr = Ap(Ap(Atom("galaxy"), state), event)
+    expr: Expr = Ap(Ap(Atom('galaxy'), state), event)
+    # print('##### interact expr #####', expr, sep='\n')
     res: Expr = eval(expr)
-    print(res)
+    # print('##### interact res #####', res, sep='\n')
     # Note: res will be modulatable here(consists of cons, nil and numbers only)
-    flag, new_state, date = GET_LIST_ITEMS_FROM_EXPR(res)
+    flag, new_state, data = GET_LIST_ITEMS_FROM_EXPR(res)
     if as_num(flag) == 0:
         return (new_state, data)
     return interact(new_state, SEND_TO_ALIEN_PROXY(data))
 
 
-def eval(expr: Expr) -> Expr:
-    if expr.evaluated is None:
+def eval(expr: Expr, indent='') -> Expr:
+    #print(indent+'===== eval expr =====', indent + str(expr), sep='\n')
+    if expr.evaluated is not None:
+        #print(indent+'-> evaluated', expr)
         return expr.evaluated
     initial: Expr = expr
     while True:
-        result: Expr = try_eval(expr)
+        result: Expr = try_eval(expr, indent+'    ')
+        #print(indent+'-> result', result)
         if result == expr:
             initial.evaluated = result
+            #print(indent+'-> tried', expr)
             return result
         expr = result
 
 
-def try_eval(expr: Expr) -> Expr:
-    if (expr.evaluated is not None):
+def try_eval(expr: Expr, indent='') -> Expr:
+    #print(indent+'===== try_eval expr =====', indent + str(expr), sep='\n')
+    if expr.evaluated is not None:
+        #print(indent+'-> evaluated', expr)
         return expr.evaluated
-    if (isinstance(expr, Atom) and FUNCTIONS[expr.name] is not None):
-        return FUNCTIONS[expr.name]
-    if (isinstance(expr, Ap)):
-        fun: Expr = eval(expr.Fun)
+    if isinstance(expr, Atom) and FUNCTIONS.get(expr.name) is not None:
+        return FUNCTIONS.get(expr.name)
+    if isinstance(expr, Ap):
+        #print(indent+'expr is Ap')
+        fun: Expr = eval(expr.fun, indent+'    ')
+        expr.fun = fun
+        #print(indent + 'fun:' + str(fun))
         x: Expr = expr.arg
-        if (isinstance(fun, Atom)):
-            if (fun.name == "neg"):
-                return Atom(-as_num(eval(x)))
-            if (fun.name == "i"):
+        if isinstance(fun, Atom):
+            if fun.name == 'neg':
+                return Atom(-as_num(eval(x, indent+'    ')))
+            if fun.name == 'inc':
+                return Atom(as_num(eval(x, indent+'    '))+1)
+            if fun.name == 'dec':
+                return Atom(as_num(eval(x, indent+'    '))-1)
+            if fun.name == 'i':
                 return x
-            if (fun.name == "nil"):
+            if fun.name == 'nil':
                 return t
-            if (fun.name == "isnil"):
+            if fun.name == 'isnil':
                 return Ap(x, Ap(t, Ap(t, f)))
-            if (fun.name == "car"):
+            if fun.name == 'car':
                 return Ap(x, t)
-            if (fun.name == "cdr"):
+            if fun.name == 'cdr':
                 return Ap(x, f)
-            if (isinstance(fun, Ap)):
-                fun2: Expr = eval(fun.Fun)
-                y: Expr = fun.arg
-                if (isinstance(fun2, Atom)):
-                    if (fun2.name == "t"):
-                        return y
-                    if (fun2.name == "f"):
-                        return x
-                    if (fun2.name == "add"):
-                        return Atom(as_num(eval(x)) + as_num(eval(y)))
-                    if (fun2.name == "mul"):
-                        return Atom(as_num(eval(x)) * as_num(eval(y)))
-                    if (fun2.name == "div"):
-                        return Atom(as_num(eval(y)) / as_num(eval(x)))
-                    if (fun2.name == "lt"):
-                        return t if as_num(eval(y)) < as_num(eval(x)) else f
-                    if (fun2.name == "eq"):
-                        return t if as_num(eval(x)) == as_num(eval(y)) else f
-                    if (fun2.name == "cons"):
-                        return eval_cons(y, x)
-                    if (isinstance(fun2, Ap)):
-                        fun3: Expr = eval(fun2.Fun)
-                        z: Expr = fun2.arg
-                        if (isinstance(fun3, Atom)):
-                            if (fun3.name == "s"):
-                                return Ap(Ap(z, x), Ap(y, x))
-                            if (fun3.name == "c"):
-                                return Ap(Ap(z, x), y)
-                            if (fun3.name == "b"):
-                                return Ap(z, Ap(y, x))
-                            if (fun3.name == "cons"):
-                                return Ap(Ap(x, z), y)
+        if isinstance(fun, Ap):
+            #print(indent+'>>>>> fun is Ap')
+            fun2: Expr = eval(fun.fun, indent+'    ')
+            expr.fun.fun = fun2
+            #print(indent + 'fun2:' + str(fun2))
+            y: Expr = fun.arg
+            if isinstance(fun2, Atom):
+                if fun2.name == 't':
+                    return y
+                if fun2.name == 'f':
+                    return x
+                if fun2.name == 'add':
+                    return Atom(as_num(eval(x, indent+'    ')) + as_num(eval(y, indent+'    ')))
+                if fun2.name == 'mul':
+                    return Atom(as_num(eval(x, indent+'    ')) * as_num(eval(y, indent+'    ')))
+                if fun2.name == 'div':
+                    return Atom(as_num(eval(y, indent+'    ')) / as_num(eval(x, indent+'    ')))
+                if fun2.name == 'lt':
+                    return t if as_num(eval(y, indent+'    ')) < as_num(eval(x, indent+'    ')) else f
+                if fun2.name == 'eq':
+                    return t if as_num(eval(x, indent+'    ')) == as_num(eval(y, indent+'    ')) else f
+                if fun2.name == 'cons':
+                    return eval_cons(y, x)
+            if isinstance(fun2, Ap):
+                fun3: Expr = eval(fun2.fun, indent+'    ')
+                expr.fun.fun.fun = fun3
+                z: Expr = fun2.arg
+                if isinstance(fun3, Atom):
+                    if fun3.name == 's':
+                        return Ap(Ap(z, x), Ap(y, x))
+                    if fun3.name == 'c':
+                        return Ap(Ap(z, x), y)
+                    if fun3.name == 'b':
+                        return Ap(z, Ap(y, x))
+                    if fun3.name == 'cons':
+                        return Ap(Ap(x, z), y)
+    #print(indent+'-> unupdated', expr)
     return expr
 
 
 def eval_cons(a: Expr, b: Expr) -> Expr:
-    res: Expr = Ap(Ap(cons, eval(a), eval(b)))
+    res: Expr = Ap(Ap(cons, eval(a)), eval(b))
     res.evaluated = res
     return res
 
@@ -130,11 +170,14 @@ def eval_cons(a: Expr, b: Expr) -> Expr:
 def as_num(n: Expr) -> int:
     if isinstance(n, Atom):
         return int(n.name)
-    raise ValueError("not a number")
+    raise ValueError('not a number')
 
 
 def parse_formula(tokens: List[str]) -> Expr:
     def dfs(index: int) -> Tuple[int, Expr]:
+        if index >= len(tokens):
+            # incomipeted ap formula
+            return None
         if tokens[index] == 'ap':
             used_index, fun = dfs(index+1)
             used_index, arg = dfs(used_index+1)
@@ -150,8 +193,9 @@ def parse_functions(lines: List[str]) -> Dict[str, Expr]:
     for line in lines:
         tokens = line.split()
         function_name = tokens[0]
+        # print(tokens)
         expr = parse_formula(tokens[2:])
-
+        # print(expr)
         functions[function_name] = expr
     return functions
 
@@ -161,18 +205,37 @@ def main():
     global FUNCTIONS
     with open(sys.argv[1]) as galaxy:
         FUNCTIONS = parse_functions(galaxy.readlines())
-    for k, v in sorted(FUNCTIONS.items(), key=lambda x: x[0]):
-        print(k, v)
+    with open('functions.tsv', 'w') as fout:
+        for k, v in sorted(FUNCTIONS.items(), key=lambda x: x[0]):
+            fout.write('{}\t{}\n'.format(k, str(v)))
 
     state: Expr = nil
     vector: Vect = Vect(0, 0)
+    click = Ap(Ap(cons, Atom(vector.x)), Atom(vector.y))
+    expr: Expr = Ap(Ap(Atom('galaxy'), state), click)
+
     while True:
-        click: Expr = Ap(Ap(cons, Atom(vector.x)), Atom(vector.y))
-        (new_state, images) = interact(state, click)
-        PRINT_IMAGES(images)
-        vector = REQUEST_CLICK_FROM_USER()
-        state = new_state
+        # repl
+        print('> ', end='')
+        tokens = input().split()
+        try:
+            expr = parse_formula(tokens)
+            print(expr)
+            print(eval(expr))
+        except TypeError as e:
+            print(e)
+    # print('##### interact expr #####', expr, sep='\n')
+    res: Expr = eval(expr)
+    # print('##### interact res #####', res, sep='\n')
+    # while True:
+    #     click: Expr = Ap(Ap(cons, Atom(vector.x)), Atom(vector.y))
+    #     #print(click)
+    #     (new_state, images) = interact(state, click)
+    #     exit()
+    #     #print_IMAGES(images)
+    #     vector = REQUEST_CLICK_FROM_USER()
+    #     state = new_state
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
