73c4ba95a18183a90fe649e43fd0e36b00a63890
matsu7874 <mtsmtkmt@gmail.com>
2020-07-19 12:28:56 +0900

    can parse file

--- app/evaluator.py
+++ app/evaluator.py
@@ -1,20 +1,30 @@
-from typing import Optional, Dict, Tuple
+import sys
+from typing import Optional, Dict, Tuple, List
+sys.setrecursionlimit(200000)
 
 
 class Expr:
-    def __init__(self, evaluated: Optional[Expr] = None):
+    def __init__(self, evaluated: Optional['Expr'] = None):
         self.evaluated = evaluated
 
 
 class Atom(Expr):
     def __init__(self, name: str):
+        super().__init__()
         self.name = name
+
+    def __str__(self):
+        return self.name
 
 
 class Ap(Expr):
     def __init__(self, fun: Expr, arg: Expr):
+        super().__init__()
         self.fun = fun
         self.arg = arg
+
+    def __str__(self):
+        return 'Ap({},{})'.format(self.fun, self.arg)
 
 
 class Vect:
@@ -28,27 +38,14 @@ t = Atom("t")
 f = Atom("f")
 nil = Atom("nil")
 
-functions: Dict[str, Expr] = PARSE_FUNCTIONS("galaxy.txt")
-
-# See https://message-from-space.readthedocs.io/en/latest/message39.html
-
-
-def main():
-    state: Expr = nil
-    vector: Vect = Vect(0, 0)
-    while True:
-        click: Expr = Ap(Ap(cons, Atom(vector.x)), Atom(vector.y))
-        var(new_state, images) = interact(state, click)
-        PRINT_IMAGES(images)
-        vector = REQUEST_CLICK_FROM_USER()
-        state = new_state
-
-# See https://message-from-space.readthedocs.io/en/latest/message38.html
+FUNCTIONS: Dict[str, Expr] = dict()
 
 
 def interact(state: Expr,  event: Expr) -> Tuple[Expr, Expr]:
+    # See https://message-from-space.readthedocs.io/en/latest/message38.html
     expr: Expr = Ap(Ap(Atom("galaxy"), state), event)
     res: Expr = eval(expr)
+    print(res)
     # Note: res will be modulatable here(consists of cons, nil and numbers only)
     flag, new_state, date = GET_LIST_ITEMS_FROM_EXPR(res)
     if as_num(flag) == 0:
@@ -71,8 +68,8 @@ def eval(expr: Expr) -> Expr:
 def try_eval(expr: Expr) -> Expr:
     if (expr.evaluated is not None):
         return expr.evaluated
-    if (isinstance(expr, Atom) and functions[expr.name] is not None):
-        return functions[expr.name]
+    if (isinstance(expr, Atom) and FUNCTIONS[expr.name] is not None):
+        return FUNCTIONS[expr.name]
     if (isinstance(expr, Ap)):
         fun: Expr = eval(expr.Fun)
         x: Expr = expr.arg
@@ -132,8 +129,49 @@ def eval_cons(a: Expr, b: Expr) -> Expr:
 
 def as_num(n: Expr) -> int:
     if isinstance(n, Atom):
-        return PARSE_NUMBER(n.name)
+        return int(n.name)
     raise ValueError("not a number")
+
+
+def parse_formula(tokens: List[str]) -> Expr:
+    def dfs(index: int) -> Tuple[int, Expr]:
+        if tokens[index] == 'ap':
+            used_index, fun = dfs(index+1)
+            used_index, arg = dfs(used_index+1)
+            return used_index, Ap(fun, arg)
+        else:
+            return index, Atom(tokens[index])
+
+    return dfs(0)[1]
+
+
+def parse_functions(lines: List[str]) -> Dict[str, Expr]:
+    functions: Dict[str, Expr] = dict()
+    for line in lines:
+        tokens = line.split()
+        function_name = tokens[0]
+        expr = parse_formula(tokens[2:])
+
+        functions[function_name] = expr
+    return functions
+
+
+def main():
+    # See https://message-from-space.readthedocs.io/en/latest/message39.html
+    global FUNCTIONS
+    with open(sys.argv[1]) as galaxy:
+        FUNCTIONS = parse_functions(galaxy.readlines())
+    for k, v in sorted(FUNCTIONS.items(), key=lambda x: x[0]):
+        print(k, v)
+
+    state: Expr = nil
+    vector: Vect = Vect(0, 0)
+    while True:
+        click: Expr = Ap(Ap(cons, Atom(vector.x)), Atom(vector.y))
+        (new_state, images) = interact(state, click)
+        PRINT_IMAGES(images)
+        vector = REQUEST_CLICK_FROM_USER()
+        state = new_state
 
 
 if __name__ == "__main__":
