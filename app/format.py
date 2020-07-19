14f18fbbe7e5d97382ad2480a60f4d009caf2261
matsu7874 <mtsmtkmt@gmail.com>
2020-07-19 17:35:14 +0900

    improve formatter

--- app/format.py
+++ app/format.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def fmt(s):
     s = s.replace(', ', ',') + '.'
     dep = 0
@@ -9,18 +12,20 @@ def fmt(s):
     while head < n:
         if s[head] == '(':
             k = 0
-            for i in range(1, min(10, n-head)):
+            for i in range(1, min(30, n-head)):
                 if s[head+i] == '(':
                     break
                 if s[head+i] == ')':
-                    k = i+1
+                    k = i
+                    if s[head+i+1] == ',':
+                        k = i+1
                     break
             lines.append(''.join([
                 base_indent*dep,
                 s[tail:head+1+k],
             ]))
-            tail = head+1+k
-            head = tail
+            head += 1+k
+            tail = head
             if k == 0:
                 dep += 1
         elif s[head] == ',':
@@ -28,25 +33,29 @@ def fmt(s):
                 base_indent*dep,
                 s[tail:head+1],
             ]))
-            tail = head+1
+            head += 1
+            tail = head
         elif s[head] == ')':
-            lines.append(''.join([
-                base_indent*dep,
-                s[tail:head],
-            ]))
+            if tail < head:
+                lines.append(''.join([
+                    base_indent*dep,
+                    s[tail:head],
+                ]))
+                tail = head
             dep -= 1
-            k = 2 if head+1 < n and s[head+1] == ',' else 1
+            k = 2 if (head+1 < n and s[head+1] == ',') else 1
             lines.append(''.join([
                 base_indent*dep,
                 s[head:head+k],
             ]))
-            tail = head+k
-            head = tail
-        head += 1
+            head += k
+            tail = head
+        else:
+            head += 1
     if tail + 1 < head:
         lines.append(s[tail:head])
 
-    return '\n'.join(lines)
+    return '\n'.join([line for line in lines if line])
 
 
 def main():
